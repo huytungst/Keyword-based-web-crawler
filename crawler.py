99456146e79a44d8ab5365105a4bc169860969d4
huytungst <42987473+huytungst@users.noreply.github.com>
2019-07-21 17:30:54 +0900

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,27 @@
+from googlesearch import search
+import urllib.request
+from urllib.request import Request, urlopen
+from inscriptis import get_text
+import ssl
+
+keyword = input('What are you searching for? ')
+num = int(input ('Number of search results? '))
+
+allresults = ''
+
+with open("TEXT.txt", "w") as f:
+  for url in search(keyword, stop=num):
+    try:
+      ssl._create_default_https_context = ssl._create_unverified_context
+      req = Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'})
+      html = urllib.request.urlopen(req).read().decode('utf-8', errors='ignore')
+      text = get_text(html)
+      result = url+'\n'+text+'\n************************************\n'
+      f.write(allresults.join(result))
+      print (url +'\n Done.')
+    except urllib.request.HTTPError: 
+      print (url+'\n HTTP Error. Can not open this link.')
+print ('\nEND OF SCRAPING. DOWNLOAD THE TEXT FILE FOR THE RESULTS.')
+
+from google.colab import files
+files.download('TEXT.txt') 
